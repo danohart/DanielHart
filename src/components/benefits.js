fc0fd3db0deb5230bf64952c21dc2a9a1d56418d
Daniel Hart <me@danielhart.co>
2020-01-03 16:50:49 -0600

    Wrap benefits in paragraph tag

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -26,10 +26,12 @@ const Benefits = () => (
       </Row>
       <Col className="benefits-description">
         <h3>Mobile Friendly</h3>
-        In 2019, Black Friday through Cyber Monday, 49% of online orders came
-        from a mobile phone. The best mobile experience for your customer is not
-        only important, but vital for growth and is always kept in mind with any
-        of our&nbsp;clients.
+        <p>
+          In 2019, Black Friday through Cyber Monday, 49% of online orders came
+          from a mobile phone. The best mobile experience for your customer is
+          not only important, but vital for growth and is always kept in mind
+          with any of our&nbsp;clients.
+        </p>
       </Col>
     </Col>
     <Col xs={12} sm={12} md={4} lg={4} className="benefits-card">
@@ -49,9 +51,11 @@ const Benefits = () => (
       </Row>
       <Col className="benefits-description">
         <h3>Data Driven</h3>
-        Through years of experience and testing, making design decisions based
-        on what your customer expects from your website, increasing sales and
-        brand loyalty that keeps people coming back to&nbsp;you.
+        <p>
+          Through years of experience and testing, making design decisions based
+          on what your customer expects from your website, increasing sales and
+          brand loyalty that keeps people coming back to&nbsp;you.
+        </p>
       </Col>
     </Col>
     <Col xs={12} sm={12} md={4} lg={4} className="benefits-card">
@@ -74,10 +78,12 @@ const Benefits = () => (
       </Row>
       <Col className="benefits-description">
         <h3>Optimal Speed</h3>
-        Your website has roughly 4 seconds before a customer decides to go
-        elsewhere if what they see isn't interesting to them. Creating snappy,
-        quick to use websites is important to keeping your customer on
-        your&nbsp;site.
+        <p>
+          Your website has roughly 4 seconds before a customer decides to go
+          elsewhere if what they see isn't interesting to them. Creating snappy,
+          quick to use websites is important to keeping your customer on
+          your&nbsp;site.
+        </p>
       </Col>
     </Col>
   </Row>
