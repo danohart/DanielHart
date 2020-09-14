4aa9b7d239fc1303a427d16621ab5774297dec95
Daniel Hart <me@danielhart.co>
2020-09-14 14:22:34 -0500

    Add gatsby source filesystem back in

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/static`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
