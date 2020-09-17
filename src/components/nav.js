0455353c9d923b51fcf8a28d4ed7d83dc0e3199b
Daniel Hart <me@danielhart.co>
2020-09-16 19:48:31 -0500

    Add link to blog, fix up excerpt text

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,20 +3,20 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 const Nav = () => (
-  <Row className="navigation">
-    <Col>
+  <Row className="navigation justify-content-center">
+    <Col xs={4} sm={2} md={2} lg={2}>
       <Link to="/">Home</Link>
     </Col>
-    <Col>
+    <Col xs={4} sm={2} md={2} lg={2}>
       <Link to="/work">Work</Link>
     </Col>
-    <Col>
+    <Col xs={4} sm={2} md={2} lg={2}>
       <Link to="/about">About</Link>
     </Col>
-    <Col>
+    <Col xs={4} sm={2} md={2} lg={2}>
       <Link to="/hire">Hire</Link>
     </Col>
-    <Col>
+    <Col xs={4} sm={2} md={2} lg={2}>
       <Link to="/blog">Blog</Link>
     </Col>
   </Row>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,21 +5,36 @@ import Seo from '../components/seo';
 import { Link } from 'gatsby';
 
 export default function BlogHome({ data }) {
+  console.log(data.allWordpressPost.edges[2].node.excerpt.length);
+
+  function shortenExcerpt(text) {
+    if (text.length > 330) return text.slice(0, -16) + '...';
+    return text;
+  }
   return (
     <Layout>
       <Seo title={'Blog'} />
-      <h1>Blog</h1>
-      <h2>
-        On topics of{' '}
-        {data.allWordpressPost.edges.map(({ node }) => (
-          <div>{node.tags}</div>
-        ))}
-      </h2>
+      <Row className="blog-header">
+        <Col>
+          <h1 className="blog-header-title">Blog</h1>
+          <h2 className="blog-header-subtitle">
+            On topics of Web Design, Travel, And Public Transit
+          </h2>
+        </Col>
+      </Row>
+
       <Row className="card-wrapper">
         {data.allWordpressPost.edges.map(({ node }) => (
           <Col xs={12} sm={12} md={12} lg={12} className="card">
-            <h2>{node.title}</h2>
-            <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            <h2>
+              <Link to={`/blog/${node.slug}`}>{node.title}</Link>
+            </h2>
+            <div
+              className="card-excerpt"
+              dangerouslySetInnerHTML={{
+                __html: shortenExcerpt(node.excerpt),
+              }}
+            />
             <Row>
               <Col
                 xs={{ span: 10, offset: 1 }}
