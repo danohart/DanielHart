290622265745ded243daeea9b487d9567830a4b4
Daniel Hart <me@danielhart.co>
2021-07-14 11:13:11 -0500

    Fix grammar errors on uses page

---
+++ src/content/usesContent.js
@@ -0,0 +1,67 @@
+export const usesContent = {
+  category: [
+    {
+      title: 'Software',
+      items: [
+        {
+          name: 'VS Code',
+          description: 'Code editor',
+          link: 'https://code.visualstudio.com/',
+          subItem: {
+            name: 'Theme',
+            description: "SynthWave '84",
+            link:
+              'https://marketplace.visualstudio.com/items?itemName=RobbOwen.synthwave-vscode',
+          },
+        },
+        {
+          name: 'Hyper',
+          description: 'A colorful terminal with useful plugins',
+          link: 'https://hyper.is/',
+          subItem: {
+            name: 'Shell',
+            description: 'Oh My Zsh',
+            link: 'https://ohmyz.sh/',
+          },
+        },
+        {
+          name: 'GitHub Desktop',
+          description: 'A graphical user interface for Git repositories',
+          link: 'https://desktop.github.com/',
+        },
+        {
+          name: 'Droplr',
+          description:
+            'A screenshot app that automatically creates a sharable link',
+          link: 'https://droplr.com/',
+        },
+      ],
+    },
+    {
+      title: 'Hardware',
+      items: [
+        {
+          name: 'MacBook Pro 13"',
+          description: '2019, 2.4 GHz Quad-Core Intel i5, 16GB RAM',
+          link: 'https://apple.com/',
+        },
+        {
+          name: 'Apple Magic Keyboard and Trackpad',
+          description:
+            'Version 1 for both items as they come cheap but work very well',
+          link: 'https://apple.com/',
+        },
+        {
+          name: 'Nexstand',
+          description: 'Laptop stand to save my neck',
+          link: 'http://www.nexstand.com/',
+        },
+        {
+          name: 'Anker 6-in-1 USB-C Adapter',
+          description: 'HDMI, Ethernet, and USB-A adaptation',
+          link: 'https://www.amazon.com/dp/B08C9HZ5YT/',
+        },
+      ],
+    },
+  ],
+};
--- src/pages/uses.js
+++ src/pages/uses.js
@@ -3,125 +3,52 @@ import { Row, Col } from 'react-bootstrap';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const softwareContent = [
-  {
-    name: 'VS Code',
-    description: 'Code editor',
-    link: 'https://code.visualstudio.com/',
-    subItem: {
-      name: 'Theme',
-      description: "SynthWave '84",
-      link:
-        'https://marketplace.visualstudio.com/items?itemName=RobbOwen.synthwave-vscode',
-    },
-  },
-  {
-    name: 'Hyper',
-    description: 'A colorful terminal with useful plugins',
-    link: 'https://hyper.is/',
-    subItem: {
-      name: 'Shell',
-      description: 'Oh My Zsh',
-      link: 'https://ohmyz.sh/',
-    },
-  },
-  {
-    name: 'GitHub Desktop',
-    description: 'A graphical user interface for Git repositories',
-    link: 'https://desktop.github.com/',
-  },
-  {
-    name: 'Droplr',
-    description: 'A screenshot app that automatically creates a sharable link',
-    link: 'https://droplr.com/',
-  },
-];
-
-const hardwareContent = [
-  {
-    name: 'MacBook Pro "13',
-    description: '2019, 2.4 GHz Quad-Core Intel i5, 16GB RAM',
-    link: 'https://apple.com/',
-  },
-  {
-    name: 'Apple Magic Keyboard and Trackpad',
-    description:
-      'Version 1 for both items as they come cheap but work very well',
-    link: 'https://apple.com/',
-  },
-  {
-    name: 'Nexstand',
-    description: 'Laptop stand to save my neck',
-    link: 'http://www.nexstand.com/',
-  },
-  {
-    name: 'Anker 6-in-1 USB-C Adapter',
-    description: 'HDMI, Ethernet, and USB-A adaptation',
-    link: 'https://www.amazon.com/dp/B08C9HZ5YT/',
-  },
-];
+// Content
+import { usesContent } from '../content/usesContent';
 
 export default function Uses() {
   return (
     <Layout>
-      <SEO title="Daniel Uses" />
+      <SEO
+        title="Uses"
+        description="List of software and hardware for web development and travel"
+      />
       <Row>
         <Col>
           <h1>What I Use</h1>
           <h2>as a frontend web developer &amp; designer</h2>
-          <h3>Software</h3>
-          {softwareContent.map(item => (
-            <ul>
-              <li>
-                <a href={item.link} target="_blank" rel="noreferrer">
-                  {item.name}
-                </a>{' '}
-                - {item.description}
-                {item.subItem ? (
-                  <ul>
+          {usesContent.category.map(content => (
+            <Row key={content.title}>
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <h3>{content.title}</h3>
+              </Col>
+              <Col>
+                {content.items.map(item => (
+                  <ul key={item.name}>
                     <li>
-                      {item.subItem.name} -
-                      <a
-                        href={item.subItem.link}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        &nbsp;{item.subItem.description}
-                      </a>
+                      <a href={item.link} target="_blank" rel="noreferrer">
+                        {item.name}
+                      </a>{' '}
+                      - {item.description}
+                      {item.subItem ? (
+                        <ul>
+                          <li>
+                            {item.subItem.name} -
+                            <a
+                              href={item.subItem.link}
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              &nbsp;{item.subItem.description}
+                            </a>
+                          </li>
+                        </ul>
+                      ) : null}
                     </li>
                   </ul>
-                ) : null}
-              </li>
-            </ul>
-          ))}
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h3>Hardware</h3>
-          {hardwareContent.map(item => (
-            <ul>
-              <li>
-                <a href={item.link} target="_blank" rel="noreferrer">
-                  {item.name}
-                </a>{' '}
-                - {item.description}
-                {item.subItem ? (
-                  <ul>
-                    <li>
-                      {item.subItem.name} -
-                      <a
-                        href={item.subItem.link}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        &nbsp;{item.subItem.description}
-                      </a>
-                    </li>
-                  </ul>
-                ) : null}
-              </li>
-            </ul>
+                ))}
+              </Col>
+            </Row>
           ))}
         </Col>
       </Row>
