263c8f96c37039a986236124ee1cc971a49fa9e3
Daniel Hart <me@danielhart.co>
2022-01-17 17:32:54 -0600

    Create usesContent

---
+++ content/usesContent.js
@@ -0,0 +1,114 @@
+export const usesContent = {
+  category: [
+    {
+      title: 'Software',
+      items: [
+        {
+          name: 'VS Code',
+          description: 'Code editor',
+          link: 'https://code.visualstudio.com/',
+          subItem: {
+            name: 'Theme',
+            description: "SynthWave '84",
+            link:
+              'https://marketplace.visualstudio.com/items?itemName=RobbOwen.synthwave-vscode',
+          },
+        },
+        {
+          name: 'Hyper',
+          description: 'A colorful terminal with useful plugins',
+          link: 'https://hyper.is/',
+          subItem: {
+            name: 'Shell',
+            description: 'Oh My Zsh',
+            link: 'https://ohmyz.sh/',
+          },
+        },
+        {
+          name: 'GitHub Desktop',
+          description: 'A graphical user interface for Git repositories',
+          link: 'https://desktop.github.com/',
+        },
+        {
+          name: 'Droplr',
+          description:
+            'A screenshot app that automatically creates a sharable link',
+          link: 'https://droplr.com/',
+        },
+      ],
+    },
+    {
+      title: 'Hardware',
+      items: [
+        {
+          name: 'MacBook Pro 13"',
+          description: '2019, 2.4 GHz Quad-Core Intel i5, 16GB RAM',
+          link: 'https://apple.com/',
+        },
+        {
+          name: 'Apple Magic Keyboard and Trackpad',
+          description:
+            'Version 1 for both items as they come cheap but work very well',
+          link: 'https://apple.com/',
+        },
+        {
+          name: 'Nexstand',
+          description: 'Laptop stand to save my neck',
+          link: 'http://www.nexstand.com/',
+        },
+        {
+          name: 'Anker 6-in-1 USB-C Adapter',
+          description: 'HDMI, Ethernet, and USB-A adaptation',
+          link: 'https://www.amazon.com/dp/B08C9HZ5YT/',
+        },
+      ],
+    },
+    {
+      title: 'Office',
+      items: [
+        {
+          name: 'Herman Miller Sayl Chair',
+          description:
+            'A smaller work chair for my home office with a rubberized back for breathability.',
+          link:
+            'https://www.hermanmiller.com/products/seating/office-chairs/sayl-chairs/',
+        },
+        {
+          name: 'LG 27" UltraGear Monitor',
+          description: '27 inch secondary monitor when in the home office',
+          link: 'https://www.lg.com/us/monitors/lg-27gl83a-b-gaming-monitor',
+          subItem: {
+            name: 'Resolution',
+            description: '2560 x 1440, 144Hz refresh rate',
+            link: 'https://www.lg.com/us/monitors/lg-27gl83a-b-gaming-monitor',
+          },
+        },
+      ],
+    },
+    {
+      title: 'Carry',
+      items: [
+        {
+          name: 'Mission Workshop Monty',
+          description:
+            'A medium-sized(21L) messenger bag that fits most of my work items',
+          link: 'https://missionworkshop.com/products/the-monty-messenger-bag',
+        },
+        {
+          name: 'Maxpedition Mini Pocket Organizer',
+          description:
+            'Small organizer for USB thumb drives, pens, battery packs, adapters, and cables',
+          link:
+            'https://www.maxpedition.com/products/mini-pocket-organizer?variant=39636203537',
+        },
+        {
+          name: 'Pawtec Keyboard and Trackpad Sleeve',
+          description:
+            'Soft case for protection and an extra pocket for batteries and cables',
+          link:
+            'https://shop.pawtec.com/collections/sleeves/products/pawtec-sleeve-for-apple-magic-keyboard-with-storage-pocket',
+        },
+      ],
+    },
+  ],
+};
