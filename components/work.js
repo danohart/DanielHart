a1c01d3c5ce422c13d3ad7cc64035410980ef17d
Daniel Hart <me@danielhart.co>
2022-01-26 15:34:45 -0600

    Update Work component layout

--- components/work.js
+++ components/work.js
@@ -2,34 +2,33 @@ import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 
 export default function Work({ pages }) {
-  function truncate(html) {
-    let tmp = document.createElement('DIV');
-    tmp.innerHTML = html;
-
-    return tmp.textContent || tmp.innerText || '';
-  }
-
   return (
     <Row className="card-wrapper">
       {pages.map((page) => (
-        <Col xs={12} sm={12} md={6} lg={6} key={page.id}>
+        <Col xs={12} sm={12} md={12} lg={12} key={page.id}>
           <div className="card">
             <h2>{page.title.rendered}</h2>
-            <div className="card-image">
-              {!page.featured_media ? (
-                <img
-                  src={`https://via.placeholder.com/728x120.png?text=Photo`}
-                />
-              ) : (
-                <img
-                  style={{
-                    maxHeight: 'calc(50vh - 6.5rem)',
-                  }}
-                  src={page._embedded['wp:featuredmedia'][0].link}
-                />
-              )}
-            </div>
-            {truncate(page.content.rendered)}
+            <Row>
+              <Col xs={12} sm={12} md={6} lg={6} className="card-image">
+                {!page.featured_media ? (
+                  <img
+                    src={`https://via.placeholder.com/728x120.png?text=Photo`}
+                  />
+                ) : (
+                  <img
+                    style={{
+                      maxHeight: 'calc(50vh - 6.5rem)',
+                    }}
+                    src={page._embedded['wp:featuredmedia'][0].link}
+                  />
+                )}
+              </Col>
+              <Col xs={12} sm={12} md={6} lg={6}>
+                <div
+                  dangerouslySetInnerHTML={{ __html: page.content.rendered }}
+                ></div>
+              </Col>
+            </Row>
           </div>
         </Col>
       ))}
