eeaacf27e83fc8767589251deb5065c1e05fe4f9
Daniel Hart <me@danielhart.co>
2022-01-28 10:04:55 -0600

    Update blog url to https

--- utils/wordpress.js
+++ utils/wordpress.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://blog.danielhart.co/wp-json/wp/v2';
+const BASE_URL = 'https://blog.danielhart.co/wp-json/wp/v2';
 
 export async function getPosts() {
   const postsRes = await fetch(BASE_URL + '/posts?_embed');
