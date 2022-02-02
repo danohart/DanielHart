3d8320ad5839485bfaf3624973fcc59783be9b95
Daniel Hart <me@danielhart.co>
2022-02-02 14:22:15 -0600

    Move URL for og:image collection

--- utils/getOgImage.js
+++ utils/getOgImage.js
@@ -3,6 +3,8 @@ const { createHash } = require('crypto');
 const fs = require('fs');
 
 async function getOgImage(path, baseUrl = 'https://danielhart.co') {
+  const url = `${baseUrl}${path}`;
+
   if (process.env.NODE_ENV === 'development') {
     return 'og image will be generated in production';
   }
@@ -12,7 +14,6 @@ async function getOgImage(path, baseUrl = 'https://danielhart.co') {
       ? 'http://localhost:3000'
       : 'https://danielhart.co';
 
-  const url = `${baseUrl}${path}`;
   const hash = createHash('md5').update(url).digest('hex');
   const browser = await playwright.launchChromium({ headless: true });
   const ogImageDir = `./public/images/og`;
