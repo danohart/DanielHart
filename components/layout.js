8b0a2ee90199786a5dd62d8bd6156f1a1582289d
Daniel Hart <me@danielhart.co>
2022-02-01 20:27:34 -0600

    Add GA tracker

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-bootstrap';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+
+if (process.env.NODE_ENV === 'production') {
+  if (typeof window !== 'undefined') {
+    ReactGA.initialize('UA-6570398-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+}
 
 const Layout = ({ children }) => {
   return (
