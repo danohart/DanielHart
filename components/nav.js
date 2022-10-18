228b87e91a5b6547710a1385d57dfa7f6aab4e58
Daniel Hart <me@danielhart.co>
2022-10-18 14:49:41 -0500

    Disable link to Blog for now

--- components/nav.js
+++ components/nav.js
@@ -16,9 +16,9 @@ const Nav = () => (
     <Col xs={4} sm={2} md={2} lg={2}>
       <Link href="/hire">Hire</Link>
     </Col>
-    <Col xs={4} sm={2} md={2} lg={2}>
+    {/* <Col xs={4} sm={2} md={2} lg={2}>
       <Link href="/blog">Blog</Link>
-    </Col>
+    </Col> */}
   </Row>
 );
 
