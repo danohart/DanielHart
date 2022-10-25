ad8e9d0686ecbd9cf10cd76171fd10871fd4d151
Daniel Hart <me@danielhart.co>
2022-10-25 10:47:19 -0500

    Update img tags for blog content

--- pages/blog/blogPosts/Madrid.js
+++ pages/blog/blogPosts/Madrid.js
@@ -1,37 +1,95 @@
-<div class="col"><h1 class="post-title">Madrid, Spain</h1><div class="row"><div class="post-date col">Written on <strong>18 Sept 2020</strong></div></div><div class="post-content">
-<p class="has-medium-font-size">Madrid takes you to the heart of the country of Spain, along with its rich history, beautiful architecture, and particularly well-taken care of community spaces and parks.</p>
+<div class="col">
+  <h1 class="post-title">Madrid, Spain</h1>
+  <div class="row">
+    <div class="post-date col">
+      Written on <strong>18 Sept 2020</strong>
+    </div>
+  </div>
+  <div class="post-content">
+    <p class="has-medium-font-size">
+      Madrid takes you to the heart of the country of Spain, along with its rich
+      history, beautiful architecture, and particularly well-taken care of
+      community spaces and parks.
+    </p>
 
+    <p class="has-medium-font-size">
+      One of the first things I noticed when spending a little time here was
+      it’s small, narrow streets. Perfect for starting your day by picking a
+      street and walking down to find some coffee.
+    </p>
 
+    <div class="wp-block-columns has-2-columns">
+      <div class="wp-block-column">
+        <div class="wp-block-image">
+          <figure class="aligncenter size-large">
+            <img
+              loading="lazy"
+              width="768"
+              height="1024"
+              src="http://blog.danielhart.co/wp-content/uploads/2019/03/Coffee-Shop-768x1024.jpg"
+              alt=""
+              class="wp-image-158"
+            />
+          </figure>
+        </div>
 
-<p class="has-medium-font-size">One of the first things I noticed when spending a little time here was it’s small, narrow streets. Perfect for starting your day by picking a street and walking down to find some coffee.</p>
+        <p class="has-medium-font-size">
+          <strong>Coffee</strong> with milk is a common treat along with toast
+          with an interesting topping, a seasoned tomato paste spread all over
+          delicious bread. Both items are seemingly around every corner.{' '}
+        </p>
+      </div>
 
+      <div class="wp-block-column">
+        <div class="wp-block-image">
+          <figure class="aligncenter size-large">
+            <img
+              loading="lazy"
+              width="768"
+              height="1024"
+              src="http://blog.danielhart.co/wp-content/uploads/2019/03/La-Ardosa-Bar-2-768x1024.jpg"
+              alt=""
+              class="wp-image-180"
+            />
+          </figure>
+        </div>
 
+        <p class="has-medium-font-size">
+          Bars are aplenty. And Spanish red wine is absolutely my favorite. And
+          it is CHEAP! Most bars have limited options for drinking, 1-2 types of
+          beers, and a few bottles of wine, but they’re all excellent choices.
+        </p>
+      </div>
+    </div>
 
-<div class="wp-block-columns has-2-columns">
-<div class="wp-block-column">
-<div class="wp-block-image"><figure class="aligncenter size-large"><img loading="lazy" width="768" height="1024" src="http://blog.danielhart.co/wp-content/uploads/2019/03/Coffee-Shop-768x1024.jpg" alt="" class="wp-image-158"></figure></div>
+    <h2>Spots to hit</h2>
 
-
-
-<p class="has-medium-font-size"><strong>Coffee</strong> with milk is a common treat along with toast with an interesting topping, a seasoned tomato paste spread all over delicious bread. Both items are seemingly around every corner. </p>
-</div>
-
-
-
-<div class="wp-block-column">
-<div class="wp-block-image"><figure class="aligncenter size-large"><img loading="lazy" width="768" height="1024" src="http://blog.danielhart.co/wp-content/uploads/2019/03/La-Ardosa-Bar-2-768x1024.jpg" alt="" class="wp-image-180"></figure></div>
-
-
-
-<p class="has-medium-font-size">Bars are aplenty. And Spanish red wine is absolutely my favorite. And it is CHEAP! Most bars have limited options for drinking, 1-2 types of beers, and a few bottles of wine, but they’re all excellent choices.</p>
-</div>
-</div>
-
-
-
-<h2>Spots to hit</h2>
-
-
-
-<ul><li><strong>El Tigre Sidra Bar</strong> – No frills, standing only, bar known for large tapas plates with every drink.</li><li><strong>Pez Tortilla</strong> – Small, hip spot with a few US beers on the menu with a bar full of Tortilla de Patatas(Spanish omelette) for late night food.</li><li><strong>Casa Camacho</strong> – A traditional Spanish vermouth bar which are known in Madrid.</li><li><strong>La Mordida</strong> – Colorful Mexican restaurant with art all over the walls.</li><li><strong>La Musa</strong> – Current lunch and dinner spot with a rotating menu of the day in a hip neighborhood.</li><li><strong>Tupper Ware</strong> – Late night bar/lounge with eyeball light fixtures on the ceiling to up the weird.</li></ul>
-</div></div>
+    <ul>
+      <li>
+        <strong>El Tigre Sidra Bar</strong> – No frills, standing only, bar
+        known for large tapas plates with every drink.
+      </li>
+      <li>
+        <strong>Pez Tortilla</strong> – Small, hip spot with a few US beers on
+        the menu with a bar full of Tortilla de Patatas(Spanish omelette) for
+        late night food.
+      </li>
+      <li>
+        <strong>Casa Camacho</strong> – A traditional Spanish vermouth bar which
+        are known in Madrid.
+      </li>
+      <li>
+        <strong>La Mordida</strong> – Colorful Mexican restaurant with art all
+        over the walls.
+      </li>
+      <li>
+        <strong>La Musa</strong> – Current lunch and dinner spot with a rotating
+        menu of the day in a hip neighborhood.
+      </li>
+      <li>
+        <strong>Tupper Ware</strong> – Late night bar/lounge with eyeball light
+        fixtures on the ceiling to up the weird.
+      </li>
+    </ul>
+  </div>
+</div>;
--- pages/blog/blogPosts/bigLaunchIsDead.js
+++ pages/blog/blogPosts/bigLaunchIsDead.js
@@ -1,39 +1,85 @@
-<div class="col"><h1 class="post-title">The Big Web Launch Is Dead</h1><div class="row"><div class="post-date col">Written on <strong>2 Feb 2022</strong></div></div><div class="row"><div class="col"><img src="https://blog.danielhart.co/wp-content/uploads/2020/09/marvin-meyer-SYTO3xs06fU-unsplash-1024x683.jpg"></div></div><div class="post-content">
-<p>There was time on the internet where companies would put up an “Under Construction” sign on their website, to raise anticipation of their new, and improved, web design or development efforts. But we’re finding out that people just end up never checking back for a new version of your website…killing the reason to have a big web launch.</p>
+<div class="col">
+  <h1 class="post-title">The Big Web Launch Is Dead</h1>
+  <div class="row">
+    <div class="post-date col">
+      Written on <strong>2 Feb 2022</strong>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <img src="https://blog.danielhart.co/wp-content/uploads/2020/09/marvin-meyer-SYTO3xs06fU-unsplash-1024x683.jpg" />
+    </div>
+  </div>
+  <div class="post-content">
+    <p>
+      There was time on the internet where companies would put up an “Under
+      Construction” sign on their website, to raise anticipation of their new,
+      and improved, web design or development efforts. But we’re finding out
+      that people just end up never checking back for a new version of your
+      website…killing the reason to have a big web launch.
+    </p>
 
+    <h2 id="improve-don-t-launch">Improve, don’t launch</h2>
 
+    <p>
+      If you have a website currently that you are embarking on revamping,
+      improving, or pivoting the idea completely, with the ability to make pages
+      private, or not promoting sections of your site until the time is right,
+      putting your site under construction does more harm than good, as you want
+      to always offer your visitor something. Improve your user experience as
+      the years go on, over time.
+    </p>
 
-<h2 id="improve-don-t-launch">Improve, don’t launch</h2>
+    <p>
+      A webpage isn’t going to be found by a visitor to your website unless you
+      tell them about it. So if it’s not linked somewhere, and you’re not
+      promoting it via your marketing channels and efforts, then you can keep
+      improving on that section of your site until it’s ready.
+    </p>
 
+    <h2 id="iterate-don-t-wait">Iterate, don’t wait</h2>
 
+    <p>
+      The iteration of a website can look different for an eCommerce store
+      versus a informative, educational blog. This website you’re reading on has
+      gone through major design changes through the years, and I recently just
+      debuted this new design last week, but that’s iteration. As design trends
+      and the web gets more accessible to more people, it’s good to revisit, and
+      iterate over what works and what doesn’t work for your visitors.{' '}
+    </p>
 
-<p>If you have a website currently that you are embarking on revamping, improving, or pivoting the idea completely, with the ability to make pages private, or not promoting sections of your site until the time is right, putting your site under construction does more harm than good, as you want to always offer your visitor something. Improve your user experience as the years go on, over time.</p>
+    <div class="wp-block-image">
+      <figure class="aligncenter size-large">
+        <img
+          loading="lazy"
+          width="480"
+          height="280"
+          src="http://blog.danielhart.co/wp-content/uploads/2020/09/webdesignarchive.gif"
+          alt=""
+          class="wp-image-217"
+        />
+        <figcaption>DanielHart.co Design Changes Through the Years</figcaption>
+      </figure>
+    </div>
 
+    <h2 id="starting-fresh-get-something-valuable-out-there">
+      Starting fresh? Get something valuable out there
+    </h2>
 
+    <p>
+      While you’re working on your major release to the public, because we
+      understand a website takes time, think about something you can get out
+      there right now that provides value to your visitor. Registering your
+      domain name and getting at least a one page website up has great SEO
+      benefits, as search engines such as Google and Bing trust websites that
+      have been around for awhile. The longer, the better. So the sooner you
+      have something that represents what you’re doing, even if it’s just
+      explaining what is coming and how they can learn more, will help you in
+      the long run when you finally reveal your finished product.
+    </p>
 
-<p>A webpage isn’t going to be found by a visitor to your website unless you tell them about it. So if it’s not linked somewhere, and you’re not promoting it via your marketing channels and efforts, then you can keep improving on that section of your site until it’s ready.</p>
-
-
-
-<h2 id="iterate-don-t-wait">Iterate, don’t wait</h2>
-
-
-
-<p>The iteration of a website can look different for an eCommerce store versus a informative, educational blog. This website you’re reading on has gone through major design changes through the years, and I recently just debuted this new design last week, but that’s iteration. As design trends and the web gets more accessible to more people, it’s good to revisit, and iterate over what works and what doesn’t work for your visitors. </p>
-
-
-
-<div class="wp-block-image"><figure class="aligncenter size-large"><img loading="lazy" width="480" height="280" src="http://blog.danielhart.co/wp-content/uploads/2020/09/webdesignarchive.gif" alt="" class="wp-image-217"><figcaption>DanielHart.co Design Changes Through the Years</figcaption></figure></div>
-
-
-
-<h2 id="starting-fresh-get-something-valuable-out-there">Starting fresh? Get something valuable out there</h2>
-
-
-
-<p>While you’re working on your major release to the public, because we understand a website takes time, think about something you can get out there right now that provides value to your visitor. Registering your domain name and getting at least a one page website up has great SEO benefits, as search engines such as Google and Bing trust websites that have been around for awhile. The longer, the better. So the sooner you have something that represents what you’re doing, even if it’s just explaining what is coming and how they can learn more, will help you in the long run when you finally reveal your finished product.</p>
-
-
-
-<h3 id="so-let-s-get-out-there-and-launch-err-show-something-to-the-world">So let’s get out there and launch, err…show something to the world!</h3>
-</div></div>
+    <h3 id="so-let-s-get-out-there-and-launch-err-show-something-to-the-world">
+      So let’s get out there and launch, err…show something to the world!
+    </h3>
+  </div>
+</div>;
