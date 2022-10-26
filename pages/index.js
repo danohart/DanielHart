3cd4a358c66fe5dc9b98f1373ed3c4d154d5d227
Daniel Hart <me@danielhart.co>
2022-10-26 12:53:59 -0500

    Add contact CTA on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Link from 'next/link';
 import Layout from '../components/layout';
+import { Row, Col, Button } from 'react-bootstrap';
 import Header from '../components/header';
 import Validators from '../components/validators';
 import Benefits from '../components/benefits';
@@ -8,14 +10,26 @@ import SEO from '../components/seo';
 import { workContent } from '../content/work';
 
 export default () => {
+  const CTA = (
+    <Row>
+      <Col className="text-center m-4">
+        <h3>Need a web developer for your next project?</h3>
+        <Button variant="primary" size="lg">
+          <Link href="/hire">Contact Me</Link>
+        </Button>
+      </Col>
+    </Row>
+  );
   return (
     <Layout>
       <SEO title="Custom Web Development" />
 
       <Header />
       <Validators />
+      {CTA}
       <Benefits />
       <Work pages={workContent.sites} />
+      {CTA}
     </Layout>
   );
 };
