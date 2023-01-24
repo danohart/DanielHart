ab88c19a2bfd0391d1d0c8ac6d1bd388f928f6da
Daniel Hart <me@danielhart.co>
2023-01-24 15:47:20 -0600

    Add imagery to one blog post

--- pages/ogimage.js
+++ pages/ogimage.js
@@ -18,13 +18,9 @@ export default function OGImage() {
       <div className="ogimage-image" style={styles}></div>
       <div className="ogimage-content">
         <h1 className="ogimage-title">
-          {title ? title : 'OG Image Generation Title Text'}
+          {title ? title : 'Daniel Hart Web Design'}
         </h1>
-        <p className="ogimage-excerpt">
-          {excerpt
-            ? excerpt
-            : 'This is an example of a description of the page or blog post that will be generated in the OG image component if served one.'}
-        </p>
+        <p className="ogimage-excerpt">{excerpt ? excerpt : ''}</p>
       </div>
     </div>
   );
