a95be511f00ddb185516136ad8690347df405cca
Daniel Hart <me@danielhart.co>
2025-04-07 19:48:39 -0400

    Comment out blog slugs

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,81 +1,81 @@
-import React from 'react';
-import Link from 'next/link';
-import Layout from '../../components/layout';
-import { Row, Col, Button } from 'react-bootstrap';
-import SEO from '../../components/seo';
-import { getSortedPostsData, getPost } from '../../utils/posts';
-import { getDate } from '../../utils/date';
-import getOgImage from '../../utils/getOgImage';
-import { decode } from 'he';
+// import React from 'react';
+// import Link from 'next/link';
+// import Layout from '../../components/layout';
+// import { Row, Col, Button } from 'react-bootstrap';
+// import SEO from '../../components/seo';
+// import { getSortedPostsData, getPost } from '../../utils/posts';
+// import { getDate } from '../../utils/date';
+// import getOgImage from '../../utils/getOgImage';
+// import { decode } from 'he';
 
-const BlogPostTemplate = ({ post, ogImage }) => (
-  <Layout>
-    <SEO title={post.title} description={post.title} image={ogImage} />
-    <Row className="post">
-      <Col>
-        <h1
-          className="post-title"
-          dangerouslySetInnerHTML={{ __html: post.title }}
-        />
-        <Row>
-          <Col className="post-date">
-            Written on <strong>{getDate(post.date)}</strong>
-          </Col>
-        </Row>
-        <div
-          className="post-content"
-          dangerouslySetInnerHTML={{ __html: post.content }}
-        />
-      </Col>
-    </Row>
-    <Row className="post-cta">
-      <Col xs={12} sm={12} md={4} lg={2}>
-        <img
-          src="/images/daniel-hart-web-developer.jpg"
-          alt="Daniel Hart - web developer"
-          title="Daniel Hart - web designer"
-        />
-      </Col>
-      <Col>
-        <h3 className="post-cta-title">Looking for a web developer?</h3>
-        <Row>
-          <Col>
-            <p>
-              Let me bring years of experience in web development and design to
-              your team, let's chat about your current, or next, project.
-            </p>
-            <Button>
-              <Link href="/hire">Contact Me</Link>
-            </Button>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </Layout>
-);
+// const BlogPostTemplate = ({ post, ogImage }) => (
+//   <Layout>
+//     <SEO title={post.title} description={post.title} image={ogImage} />
+//     <Row className="post">
+//       <Col>
+//         <h1
+//           className="post-title"
+//           dangerouslySetInnerHTML={{ __html: post.title }}
+//         />
+//         <Row>
+//           <Col className="post-date">
+//             Written on <strong>{getDate(post.date)}</strong>
+//           </Col>
+//         </Row>
+//         <div
+//           className="post-content"
+//           dangerouslySetInnerHTML={{ __html: post.content }}
+//         />
+//       </Col>
+//     </Row>
+//     <Row className="post-cta">
+//       <Col xs={12} sm={12} md={4} lg={2}>
+//         <img
+//           src="/images/daniel-hart-web-developer.jpg"
+//           alt="Daniel Hart - web developer"
+//           title="Daniel Hart - web designer"
+//         />
+//       </Col>
+//       <Col>
+//         <h3 className="post-cta-title">Looking for a web developer?</h3>
+//         <Row>
+//           <Col>
+//             <p>
+//               Let me bring years of experience in web development and design to
+//               your team, let's chat about your current, or next, project.
+//             </p>
+//             <Button>
+//               <Link href="/hire">Contact Me</Link>
+//             </Button>
+//           </Col>
+//         </Row>
+//       </Col>
+//     </Row>
+//   </Layout>
+// );
 
-export default BlogPostTemplate;
+// export default BlogPostTemplate;
 
-export async function getStaticPaths() {
-  const paths = await getSortedPostsData('slugs');
+// export async function getStaticPaths() {
+//   const paths = await getSortedPostsData('slugs');
 
-  return {
-    paths,
-    //this option below renders in the server (at request time) pages that were not rendered at build time
-    //e.g when a new blogpost is added to the app
-    fallback: 'blocking',
-  };
-}
+//   return {
+//     paths,
+//     //this option below renders in the server (at request time) pages that were not rendered at build time
+//     //e.g when a new blogpost is added to the app
+//     fallback: 'blocking',
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  const post = await getPost(params.slug);
-  // const ogImage = await getOgImage(`/ogimage?title=${decode(post.title)}`);
+// export async function getStaticProps({ params }) {
+//   const post = await getPost(params.slug);
+//   // const ogImage = await getOgImage(`/ogimage?title=${decode(post.title)}`);
 
-  return {
-    props: {
-      post,
-      ogImage,
-    },
-    revalidate: 10, // In seconds
-  };
-}
+//   return {
+//     props: {
+//       post,
+//       ogImage,
+//     },
+//     revalidate: 10, // In seconds
+//   };
+// }
